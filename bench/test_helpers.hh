da7697fc1f0004aefc6d2122e18036bb82e736f6
Nicolas Blin <nblin@lrde.epita.fr>
2021-11-18 10:50:52 +0100

    add comment on check buffer

--- bench/test_helpers.hh
+++ bench/test_helpers.hh
@@ -5,6 +5,8 @@
 template <typename FUNC>
 void check_buffer(cuda_tools::host_shared_ptr<int> buffer, FUNC func);
 
+// Compare device data of buffer with host data of expected
+// Retriving data host side in buffer before already performed
 void check_buffer(cuda_tools::host_shared_ptr<int> buffer,
                   cuda_tools::host_shared_ptr<int> expected);
 
