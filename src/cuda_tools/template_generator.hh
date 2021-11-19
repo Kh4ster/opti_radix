6286fe5d5d85f63cff071dbf1a2c1408790d9077
Nicolas Blin <nblin@lrde.epita.fr>
2021-11-19 16:30:08 +0100

    add char & unsigned char and better formating on template generator

--- src/cuda_tools/template_generator.hh
+++ src/cuda_tools/template_generator.hh
@@ -1,16 +1,22 @@
 #pragma once
 
-#define template_generator(CLASS_NAME, TYPE_NAME) \
+// clang-format off
+
+#define template_generator(CLASS_NAME, TYPE_NAME)                              \
     template class CLASS_NAME<TYPE_NAME>;
 
-#define template_generation(CLASS_NAME)                                            \
-    template_generator(CLASS_NAME, short)                                          \
-        template_generator(CLASS_NAME, int)                                        \
-            template_generator(CLASS_NAME, long)                                   \
-                template_generator(CLASS_NAME, long long)                          \
-                    template_generator(CLASS_NAME, unsigned short)                 \
-                        template_generator(CLASS_NAME, unsigned int)               \
-                            template_generator(CLASS_NAME, unsigned long)          \
-                                template_generator(CLASS_NAME, unsigned long long) \
-                                    template_generator(CLASS_NAME, float)          \
-                                        template_generator(CLASS_NAME, double)
+#define template_generation(CLASS_NAME)                                        \
+    template_generator(CLASS_NAME, char)                                       \
+    template_generator(CLASS_NAME, short)                                      \
+    template_generator(CLASS_NAME, int)                                        \
+    template_generator(CLASS_NAME, long)                                       \
+    template_generator(CLASS_NAME, long long)                                  \
+    template_generator(CLASS_NAME, unsigned char)                              \
+    template_generator(CLASS_NAME, unsigned short)                             \
+    template_generator(CLASS_NAME, unsigned int)                               \
+    template_generator(CLASS_NAME, unsigned long)                              \
+    template_generator(CLASS_NAME, unsigned long long)                         \
+    template_generator(CLASS_NAME, float)                                      \
+    template_generator(CLASS_NAME, double)
+
+// clang-format on
